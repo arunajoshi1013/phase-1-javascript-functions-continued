19eddca1161f1daa580b7f782cc633e0e1206b77
Aruna  Joshi <arunajoshi1013@gmail.com>
2023-03-25 16:00:25 -0700

    complete functions contd. lab

--- index.js
+++ index.js
@@ -6,3 +6,9 @@ function saturdayFun(activity = "roller-skate") {
 const mondayWork = function(act = "go to the office") {
    return `This Monday, I will ${act}.`
 }
+
+function wrapAdjective(str = "*") {
+  return function(val = "special") {
+      return `You are ${str}${val}${str}!`;
+  }
+}
